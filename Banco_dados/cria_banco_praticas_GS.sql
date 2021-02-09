9917a335a578aa305901967b57fedb2246443c20
cheracomo <cheracomo.b@gmail.com>
2021-02-09 17:02:39 -0300

    Update cria_banco_praticas_GS.sql

--- Banco_dados/cria_banco_praticas_GS.sql
+++ Banco_dados/cria_banco_praticas_GS.sql
@@ -171,7 +171,8 @@ VALUES
 (4,1,1,'1.3 - canteiro principal','Milho-algodão-fava-feijão: Canteiro com 3m de largura. Linha central de algodão (espaçamento: 1,0 m); duas linhas de milho intercalado com fava (espaçamento entre linhas 1,5m e entreplantas (milho-fava) 0,5m); feijão faseolo com espaçamento de monocultura nas bordas do canteiro (0,3 m x 0,3 m)'),
 (5,1,1,'1.4 - canteiro principal','Milho-algodão-fava-vagem: Canteiro com 3m de largura. Linha central de algodão (espaçamento: 1,0 m); duas linhas de milho intercalado com fava (espaçamento entre linhas 1,5m e entreplantas (milho-fava) 0,5m); vagem rasteira com espaçamento de monocultura nas bordas do canteiro (0,3 m x 0,3 m)'),
 (6,1,1,'entrelinha adubadora','fazer uma muvuca com as sementes na porporção de 10kg/ha de crotalária juncea, 10kg/ha de crotalaria spectabilis e 10kg/ha de mombaça'),
-(7,1,1,'entrelinha adubadora','fazer uma muvuca com as sementes na porporção de 20kg/ha de crotalária spectabilis e 10kg/ha de mombaça');
+(7,1,1,'entrelinha adubadora','fazer uma muvuca com as sementes na porporção de 20kg/ha de crotalária spectabilis e 10kg/ha de mombaça'),
+(10,1,1,'1.5 - canteiro principal','Milho-fava-feijao: Canteiro com 3m de largura. Duas linhas de milho intercalado com fava (espaçamento entre linhas 1,5m e entreplantas (milho-fava) 0,5m); duas linhas de feijão nas bordas do canteiro com espaçamento de monocultura e tres linhas entre as linhas de milho e fava (0,3 m x 0,3 m)');
 
 INSERT or REPLACE INTO especie_canteiro (id_canteiro_especie,id_canteiro,id_especie)
 VALUES
@@ -203,7 +204,11 @@ VALUES
 (21,6,10),
 
 (22,7,6),
-(23,7,10);
+(23,7,10),
+
+(32, 10, 1),
+(33, 10, 4),
+(34, 10, 8);
 
 ----------------------------------------------------------------------------------------
 INSERT or REPLACE INTO especie_vegetal (id_especie,nome_cientifico, resiliencia_especie, estrato, colheita_poda_dias, colheita_poda_anos)
@@ -234,6 +239,96 @@ VALUES
 (30, 9, 11),
 (31,9,7);
 ----------------------------------------------------------------------------------------
+
+INSERT or REPLACE INTO especie_vegetal (id_especie,nome_cientifico, resiliencia_especie, estrato, colheita_poda_dias, colheita_poda_anos)
+VALUES 
+(12, 'Anadenanthera macrocarpa', 3 , 'emergente', '-', '3'),
+(13, 'Leucaena leucocephala', 3 , 'alto', '360', '1'),
+(14, 'Licania rigida', 3 , 'medio', '-', '3'),
+(15, 'Arachis hypogaea', 3 , 'baixo', '180', '0,5'),
+(16, 'Ananas comosus', 3 , 'baixo', '360', '1'),
+(17, 'Amburana cearensis', 3 , 'alto', '-', '3'),
+(18, 'Agave sisalana', 3 , 'baixo', '-', '3 a 8'),
+(19, 'Cereus jamacaru', 3 , 'alto', '-', '3'),
+(20, 'Spondias tuberosa', 3 , 'medio', '-', '8');
+
+INSERT or REPLACE INTO nome_popular (id_nome_pop,id_especie,nome_pop)
+VALUES
+(25, 11, 'mandioca'),
+(26, 11, 'macaxeira'),
+(27, 11, 'aipim'),
+(28, 12, 'angico'),
+(29, 12, 'arapiraca'),
+(30, 12, 'curupai'),
+(31, 13, 'leucena'),
+(32, 13, 'arvore-do-conflito'),
+(33, 13, 'deserto-verde'),
+(34, 14, 'oiticica'),
+(35, 15, 'amendoim'),
+(36, 16, 'abacaxi'),
+(37, 17, 'emburana-de-cheiro'),
+(38, 17, 'emburana'),
+(39, 17, 'imburana'),
+(40, 18, 'sisal'),
+(41, 18, 'agave'),
+(42, 19, 'mandacaru'),
+(43, 20, 'umbu')
+;
+
+INSERT or replace INTO canteiro (id_canteiro,id_cult_principal,id_sistema, funcao_canteiro, descricao)
+VALUES
+(8,1,1,'1.5 - canteiro principal','Milho-mandioca-feijão: Canteiro com 3m de largura. Linha central de mandioca (espaçamento:1,5 m); duas linhas de milho intercalado com feijão de corda (espaçamento entre linhas 1,5m e entreplantas (milho-feijão de corda) 0,5m); feijão faseolo com espaçamento de monocultura nas bordas do canteiro (0,3 m x 0,3 m)'),
+(9,1,1,'1.6 - canteiro principal','Milho-mandioca-feijão-vagem: Canteiro com 3m de largura. Linha central de mandioca (espaçamento:1,5 m); duas linhas de milho intercalado com feijão de corda (espaçamento entre linhas 1,5m e entreplantas (milho-feijão de corda) 0,5m); vagem rasteira com espaçamento de monocultura nas bordas do canteiro (0,3 m x 0,3 m)'),
+(10,1,1,'1.7 - arvores do futuro','Angico-leucena-oiticica: Canteiro com 3m de largura. Linha central de angico, leucena e oiticica (espaçamento:6 m entre cada angico 6 m entre cada leucena e 3 m entre cada oiticica); duas linhas de milho intercalado com fava (espaçamento entre linhas 1,5m e entreplantas (milho-fava) 0,5m); duas linhas de abacaxi (0,5m entre plantas) e quatro linhas de amendoim (0,5 entre linhas e 0,5 m entre plantas)'),
+(11,1,1,'1.8 - arvores do futuro','Angico-leucena-umbu: Canteiro com 3m de largura. Linha central de angico, leucena e umbu (espaçamento:6 m entre cada angico 6 m entre cada leucena e 3 m entre cada umbu); duas linhas de milho intercalado com fava (espaçamento entre linhas 1,5m e entreplantas (milho-fava) 0,5m); duas linhas de abacaxi (0,5m entre plantas) e quatro linhas de amendoim (0,5 entre linhas e 0,5 m entre plantas)'),
+(12,1,1,'1.9 - arvores do futuro','Mandacaru-leucena-sisal: Canteiro com 3m de largura. Linha central de mandacaru e leucena (espaçamento:6 m entre cada leucena e 1,5 a cada mandacaru); duas linhas de milho intercalado com fava (espaçamento entre linhas 1,5m e entreplantas (milho-fava) 0,5m); duas linhas de abacaxi (0,5m entre plantas) e tres linhas de amendoim (0,5 entre linhas e 0,5 m entre plantas); duas linhas de sisal bem proximas a linha central com 0,5 m de espaçamento entre plantas'),
+(13,1,1,'1.10 - arvores do futuro','Emburana-leucena-sisal: Canteiro com 3m de largura. Linha central de emburana e leucena (espaçamento:6 m entre cada leucena e 6 m a cada emburana); duas linhas de milho intercalado com fava (espaçamento entre linhas 1,5m e entreplantas (milho-fava) 0,5m); duas linhas de abacaxi (0,5m entre plantas) e tres linhas de amendoim (0,5 entre linhas e 0,5 m entre plantas)duas linhas de sisal bem proximas a linha central com 0,5 m de espaçamento entre plantas');
+
+
+
+INSERT or REPLACE INTO especie_canteiro (id_canteiro_especie,id_canteiro,id_especie)
+VALUES
+(24, 8, 1), 
+(25, 8, 2),
+(26, 8, 11),
+(27,8,4),
+
+(28, 9, 1), 
+(29, 9, 2),
+(30, 9, 11),
+(31,9,7),
+
+(32, 10, 1), 
+(33, 10, 12),
+(34, 10, 13),
+(35,10,14),
+(36, 10, 15), 
+(37, 10, 16),
+
+(38, 11, 1), 
+(39, 11, 12),
+(40, 11, 13),
+(41, 11, 20),
+(42, 11, 15), 
+(43, 11, 16),
+
+(44, 12, 1), 
+(45, 12, 19),
+(46, 12, 13),
+(47, 12, 18),
+(48, 12, 15), 
+(49, 12, 16),
+
+(50, 13, 1), 
+(51, 13, 17),
+(52, 13, 13),
+(53, 13, 20),
+(54, 13, 15), 
+(55, 13, 16);
+
+
+
+
 
 -- Exemplo de consulta
  select id_sistema, nome_cientifico, funcao_planta
